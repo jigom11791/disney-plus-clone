416f26bb4147164ec773d2d42f169376b12018d6
jigom11791 <jigom11791@gmail.com>
2022-01-31 18:19:15 -0800

    detail page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Home from './components/Home';
+import Detail from './components/Detail';
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link 
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="detail" element={<Detail />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,37 +3,37 @@ import styled from 'styled-components';
 
 function Header() {
   return (
-      <Nav>
-          <Logo src="./images/logo.svg" />
-          <NavMenu>
-              <a>
-                  <img src="./images/home-icon.svg" alt="home icon"/>
-                  <span>HOME</span>
-              </a>
-              <a>
-                  <img src="./images/search-icon.svg" alt="home icon"/>
-                  <span>SEARCH</span>
-              </a>
-              <a>
-                  <img src="./images/watchlist-icon.svg" alt="home icon"/>
-                  <span>WATCHLIST</span>
-              </a>
-              <a>
-                  <img src="./images/original-icon.svg" alt="home icon"/>
-                  <span>ORIGINALS</span>
-              </a>
-              <a>
-                  <img src="./images/movie-icon.svg" alt="home icon"/>
-                  <span>MOVIES</span>
-              </a>
-              <a>
-                  <img src="./images/series-icon.svg" alt="home icon"/>
-                  <span>SERIES</span>
-              </a>
+        <Nav>
+            <Logo src="./images/logo.svg" />
+            <NavMenu>
+                <a>
+                    <img src="./images/home-icon.svg" alt="home icon"/>
+                    <span>HOME</span>
+                </a>
+                <a>
+                    <img src="./images/search-icon.svg" alt="home icon"/>
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src="./images/watchlist-icon.svg" alt="home icon"/>
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img src="./images/original-icon.svg" alt="home icon"/>
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src="./images/movie-icon.svg" alt="home icon"/>
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src="./images/series-icon.svg" alt="home icon"/>
+                    <span>SERIES</span>
+                </a>
 
-          </NavMenu>
-          <UserImg src="./images/user.jpg"/>
-      </Nav>
+            </NavMenu>
+            <UserImg src="./images/user.jpg"/>
+        </Nav>
   )
 }
 
